7889517569247f106aae7abd79af9f2197bcbdab
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2024-04-20 11:20:03 +0200

    add vercel ai stream

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,50 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import {
+	StreamingTextResponse,
+	AIStream,
+	type AIStreamParser,
+	formatStreamPart,
+} from 'ai';
+
+export interface Env {
+	AI: any;
+}
+
+function parseCloudflareStream(): AIStreamParser {
+	return data => {
+		const json = JSON.parse(data) as {
+			response: string;
+			p: string | null;
+		};
+		const text = json.response;
+		return formatStreamPart("text", text);
+	};
+}
+
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env) {
+		if (request.method === "OPTIONS") {
+			return new Response(null, {
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+					"Access-Control-Allow-Headers": "content-type",
+					"Access-Control-Max-Age": "86400",
+				},
+			});
+		}
+
+		const { messages } = await request.json<{ messages: { role: string; content: string }[] }>();
+
+		const cfStream = await env.AI.run("@hf/google/gemma-7b-it", {
+			messages,
+			stream: true,
+		});
+
+		const response = new Response(cfStream, { headers: { "content-type": "text/event-stream" } });
+
+		const stream = AIStream(response, parseCloudflareStream());
+
+		return new StreamingTextResponse(stream, { headers: { "Access-Control-Allow-Origin": "*" } });
 	},
 };
